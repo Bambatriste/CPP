d6fc43c893fb10736eb2209526d3c94080beb78d
Antoine EDOUARD <aedouard@e2r9p1.clusters.42paris.fr>
2022-01-22 20:49:16 +0100

    fatigue

--- CPP00/ex02/Account.cpp
+++ CPP00/ex02/Account.cpp
@@ -120,6 +120,12 @@ void	Account::displayStatus( void ) const
 
 void	Account::_displayTimestamp( void )
 {
+	time_t rawtime;
+	struct tm *timeinfo;
+	char buffer [80];
 
-
+	time(&rawtime);
+	timeinfo = localtime(&rawtime);
+	strftime(buffer, 80, "[%Y%m%d_%H%M%S] ", timeinfo);
+	std::cout << buffer;
 }
